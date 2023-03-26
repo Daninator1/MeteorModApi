c57a9c50d54b13683904a51e7d32becc88828404
Daniel Zauner <daninator.zauner@gmail.com>
2023-03-26 18:29:13 +0200

    PoC of play status positions

--- MeteorModApi/Models/PlayStatusEntry.cs
+++ MeteorModApi/Models/PlayStatusEntry.cs
@@ -1,14 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 
 namespace MeteorModApi.Models;
 
 public class PlayStatusEntry
 {
     [MinLength(1)] public required string Name { get; init; }
-
     [MinLength(1)] public required string PlayerName { get; init; }
-
     [MinLength(1)] public required string Server { get; init; }
-
+    public required double PosX { get; set; }
+    public required double PosY { get; set; }
+    public required double PosZ { get; set; }
     public DateTime Inserted { get; } = DateTime.Now;
 }
