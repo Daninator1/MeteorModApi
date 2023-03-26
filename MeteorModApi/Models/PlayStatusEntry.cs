e8b6e869386915b1c3e6986bbd3629c07afd5a33
Daniel Zauner <daninator.zauner@gmail.com>
2023-03-26 23:23:15 +0200

    small improvements, added dimension

--- MeteorModApi/Models/PlayStatusEntry.cs
+++ MeteorModApi/Models/PlayStatusEntry.cs
@@ -8,8 +8,7 @@ public class PlayStatusEntry
     [MinLength(1)] public required string Name { get; init; }
     [MinLength(1)] public required string PlayerName { get; init; }
     [MinLength(1)] public required string Server { get; init; }
-    public required double PosX { get; set; }
-    public required double PosY { get; set; }
-    public required double PosZ { get; set; }
+    public required PlayStatusPosition Position { get; init; }
+    [MinLength(1)] public required string Dimension { get; init; }
     public DateTime Inserted { get; } = DateTime.Now;
 }
---
+++ MeteorModApi/Models/PlayStatusPosition.cs
@@ -0,0 +1,8 @@
+namespace MeteorModApi.Models;
+
+public class PlayStatusPosition
+{
+    public required double X { get; init; }
+    public required double Y { get; init; }
+    public required double Z { get; init; }
+}
